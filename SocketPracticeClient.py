ed32f2f5a824b5d71d3f8ea216762421f6f17122
Matthew Harding <mmcoolj@MMCoolJ-PC.(none)>
2014-04-28 21:35:00 -0400

    Beginning to work on the multiplayer

--- SocketPracticeClient.py
+++ SocketPracticeClient.py
@@ -5,9 +5,9 @@ import socket
 def main():
 	# Going to connect to my own machine
 
-	gameClient = socket.socket()		# create socket object
-	host = '128.237.68.111'				# Desktop's current IP address
-	accessPort = 3210					# gameComsPort = 3210
+	gameClient = socket.socket()	# create socket object
+	host = '128.237.68.111'			# Desktop's current (static) IP address
+	accessPort = 3210				# gameComsPort = 3210
 
 	gameClient.connect ( (host, accessPort) )
 	maxBytesToRecieve = 1024
